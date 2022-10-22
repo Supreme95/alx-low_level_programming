cd23ef5cddd271cbc2b513f1330763caa03d558c
Supreme95 <Fatade.tosin1@gmail.com>
2022-10-21 20:20:54 -0700

    First commit for nested loop

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _isupper - Check if a letter is in uppercase
+ * main: Check the code.
+ * int c:The main number to be checked
+ *
+ * Return: 1 for upper letter or 0 for others
+ */
+
+int main(void)
+int _isupper(int c)
+{
+	if (c >= 65 && x <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
